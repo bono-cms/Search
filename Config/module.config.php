f74911b43b03f86a011243d9086260f55fdfd13f
David Yang <daworld.ny@gmail.com>
2016-07-12 21:57:35 +0500

    Fixed formatting

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,13 +1,11 @@
 <?php
- return array (
-  'module' => 
-  array (
-    'name' => 'Search',
-    'route' => 'Search:Admin:Config@indexAction',
-    'icon' => 'fa fa-search fa-5x',
-    'order' => 1,
-    'description' => 'Search module allows you to easily enable search mechanism across another modules',
-  ),
-  'per_page_count' => '2',
-  'max_description_length' => '100',
+
+return array(
+    'module' => array(
+        'name' => 'Search',
+        'route' => 'Search:Admin:Config@indexAction',
+        'icon' => 'fa fa-search fa-5x',
+        'order' => 1,
+        'description' => 'Search module allows you to easily enable search mechanism across another modules',
+    )
 );
